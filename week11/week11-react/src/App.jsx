ef29489bea04a7aae6ef414cbe47e501c57dccb9
Adrina Avila <avilaadriana46@gmail.com>
2024-04-09 13:39:52 -0700

    week11_HandsOn

---
+++ week11/week11-react/src/App.jsx
@@ -0,0 +1,12 @@
+import FirstName from "./FirstName"
+import TimeOfDay from "./TimeOfDay"
+export default function App(){
+  return(
+    <> 
+      <FirstName/><br/>
+      <TimeOfDay/>
+    </>
+  )
+}
+
+// "<> </>" empty container 
---
+++ week11/week11-react/src/TimeOfDay.jsx
@@ -0,0 +1,5 @@
+export default function TimeOfDay(){
+    return(
+        "G'day Mate"
+    )
+}
